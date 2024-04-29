11ed9c89a9c478ee61f00a2f120fe7aad2ae8d8a
vadlapravallika <145421563+vadlapravallika@users.noreply.github.com>
2024-04-29 00:59:11 -0500

    category.js

--- controller/category.js
+++ controller/category.js
@@ -1,48 +1,68 @@
 const Category = require("../models/category");
 const Product = require("../models/product");
 
+// Create a new category
 const create = (req, res, next) => {
-  let categoryName = req.body.catName;
-  let category = new Category({
+  const categoryName = req.body.catName;
+  const category = new Category({
     categoryName,
   });
-  category.save().then((data) => {
-    res.send(data);
-  });
-};
 
-const view = (req, res, next) => {
-  Category.find({}).then((data) => {
-    res.send(data);
-  });
-};
-
-const update = (req, res, next) => {
-  Category.findByIdAndUpdate(req.params.id, req.body, (err, category) => {
-    if (err) {
-      return res
-        .status(500)
-        .send({ error: "Error with updating the category recorded. " });
-    }
-    res.send(category);
-  });
-};
-
-const remove = (req, res, next) => {
-  Category.findByIdAndDelete(req.params.id, (err, category) => {
-    Product.find({}).then((data) => {
-      data.forEach((d) => {
-        if (d["category"] == req.params.id) {
-          Product.deleteOne(d, (err, r) => {
-            res.send("Deleted Successfully!");
-          });
-        }
-      });
+  category.save()
+    .then((data) => {
+      res.status(201).json(data); // Return created category with status code 201
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error creating category" });
     });
-  });
 };
 
-module.exports.create = create;
-module.exports.view = view;
-module.exports.update = update;
-module.exports.remove = remove;
+// View all categories
+const view = (req, res, next) => {
+  Category.find()
+    .then((data) => {
+      res.status(200).json(data); // Return categories with status code 200
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error fetching categories" });
+    });
+};
+
+// Update a category by ID
+const update = (req, res, next) => {
+  const categoryId = req.params.id;
+  const updateData = req.body;
+
+  Category.findByIdAndUpdate(categoryId, updateData, { new: true })
+    .then((category) => {
+      if (!category) {
+        return res.status(404).json({ error: "Category not found" });
+      }
+      res.status(200).json(category); // Return updated category with status code 200
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error updating category" });
+    });
+};
+
+// Remove a category by ID
+const remove = (req, res, next) => {
+  const categoryId = req.params.id;
+
+  Category.findByIdAndDelete(categoryId)
+    .then((category) => {
+      if (!category) {
+        return res.status(404).json({ error: "Category not found" });
+      }
+      // Remove products associated with the deleted category
+      return Product.deleteMany({ category: categoryId });
+    })
+    .then(() => {
+      res.status(200).json({ message: "Category deleted successfully" });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error deleting category" });
+    });
+};
+
+module.exports = { create, view, update, remove };
